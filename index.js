e3b2e779de7939e9726cbd9c5501ef310f5c8af9
corentin <corentin.jechoux.pro@gmail.com>
2014-03-19 10:52:05 -0400

    refactor index.js add search function to dictionary finish reader algo
    delete a 'foo' dictionary

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 var Reader = require( './lib/Reader' );
 var Dictionary = require( './lib/Dictionary' );
+var Validator = require( './lib/Validator' );
 
-var dictionaryPath = './dictionaries/fix44';
-console.log( 'Loading dictionaries:' + dictionaryPath );
 var fix44 = require( './dictionaries/fix44' );
-console.log( 'Dictionary loaded');
 
 var options = {
    dictionary: new Dictionary( fix44 )
 };
 
-var reader = new Reader( options );
+var validator = new Validator( options.dictionary );
+validator.on( 'end', function ( errors ) {
+    console.log( 'validator errors ?' );
+    for (var i = 0; i < errors.length; i++) {
+        console.error( errors[i].message );
+    }
+} );
 
-reader.read( __dirname + '/samples/message1.txt' );
+var reader = new Reader( options );
+reader.addListener( validator );
+
+// TODO input as a stream
+reader.read( __dirname + '/samples/message1.txt', function ( err ) {
+    if ( err ) {
+        console.error( err );
+    }
+} );
+
+
